f2a32f01a6c633141a9b81136a511109bdf1a4d5
ozancanonen <ozancanonen2@gmail.com>
2019-08-23 21:25:58 +0300

    Update HEllo.cs
    Hello!

--- TyringOutGitHub/Assets/HEllo.cs
+++ TyringOutGitHub/Assets/HEllo.cs
@@ -7,7 +7,7 @@ public class HEllo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print(hello);
+        Debug.Log("hello");
         
     }
 
