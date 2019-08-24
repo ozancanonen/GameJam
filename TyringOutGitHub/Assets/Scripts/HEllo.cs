0d44b3a9d1d3d48addbfe0d4186fb96815405e6a
ozancanonen <ozancanonen2@gmail.com>
2019-08-24 20:42:01 +0300

    Setted the project folders

--- TyringOutGitHub/Assets/Scripts/HEllo.cs
+++ TyringOutGitHub/Assets/Scripts/HEllo.cs
@@ -6,9 +6,9 @@ public class HEllo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
+
     {
         Debug.Log("hello, ı'm your new PET");
-        
     }
 
     // Update is called once per frame
