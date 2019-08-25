b06e3bdae5a042d0a892822061eb23de472bf5a2
ozancanonen <ozancanonen2@gmail.com>
2019-08-25 04:29:09 +0300

    I imported a temporary pixel character for us to use till we get the ones from the art team

--- GameJam/Assets/Scripts/PlayerController.cs
+++ GameJam/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed;
 
-
+    private Vector2 movement;
+    private Rigidbody2D rb;
 
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
-
-
 
     void Update()
     {
-        
+        //returns a value between -1 and 1 according to the pressed buttons that are defined for Horizontal in Unity(they are changeable)
+        //we assign these values to "movement"
+        movement.x= Input.GetAxisRaw("Horizontal");
+        movement.y= Input.GetAxisRaw("Vertical");
+    }
+
+    //we are using FixedUpdate for all physical related stuff 
+    void FixedUpdate()
+    {
+        //we make the player move according to the player Input, we multiplied the value with time to make the movement at a constant speed 
+        //not relative to fps
+        rb.MovePosition(rb.position + movement * moveSpeed*Time.fixedDeltaTime);
     }
 
 
