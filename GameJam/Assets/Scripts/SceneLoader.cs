18c066ef8a1472757875cea9d4fca6e4799ec140
ozancanonen <ozancanonen2@gmail.com>
2019-08-25 03:50:02 +0300

    Tmp imported, added basic menu, and getting started on the palyer controller

---
+++ GameJam/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadLevel(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
