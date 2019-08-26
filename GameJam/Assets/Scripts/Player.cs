b830fbb1ed474ac16c1261d3947b3b9474d89c76
ozancanonen <ozancanonen2@gmail.com>
2019-08-26 14:20:43 +0300

    Trying to experiment on particles

--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public string enemyBulletTag;
     public string horizontalMovementInputButtons;
