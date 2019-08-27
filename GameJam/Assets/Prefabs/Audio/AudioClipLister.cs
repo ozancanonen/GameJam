d199b732d9c35f57fa2dd1f1f195076fe1f5ef42
Bidmare <42129033+Bidmare@users.noreply.github.com>
2019-08-27 10:31:08 -0400

    AudioUpdate
    Added an audiomanager and a PlaySound() script.

---
+++ GameJam/Assets/Prefabs/Audio/AudioClipLister.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioClipLister : MonoBehaviour
+{
+
+    public AudioSource AudioSource;
+    public AudioClip[] AudioClips;
+    int ClipsLoaded = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void PlaySound(int ClipNumber, float Pitch, int Priority)
+    {
+        AudioSource.clip = AudioClips[ClipNumber];
+        AudioSource.pitch = Pitch;
+        AudioSource.priority = Priority;
+
+        AudioSource.Play();
+    }
+}
