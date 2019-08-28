24401d74e52d2083940ebcec58b9d61952a6a612
ozancanonen <ozancanonen2@gmail.com>
2019-08-28 23:09:09 +0300

    Animations upgraded_ startedSingleplayer

--- GameJam/Assets/Scripts/Construct.cs
+++ GameJam/Assets/Scripts/Construct.cs
@@ -74,7 +74,7 @@ public class Construct : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Wall" || col.gameObject.tag == "Player1" || col.gameObject.tag == "Player2" || col.gameObject.tag == "Destroyer");
+        if (col.gameObject.tag == "Wall" || col.gameObject.tag == "Player1" || col.gameObject.tag == "Player2" || col.gameObject.tag == "Destroyer")
             move = true;
     }
 
